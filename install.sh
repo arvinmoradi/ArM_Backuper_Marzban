308b0d5207a6fcebd2876452005c49bf96f19729
ArM <91786853+arvinmoradi@users.noreply.github.com>
2024-10-01 21:16:32 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# نمایش پیام نصب
+# Display installation message
 echo -e "\e[32mStarting the installation process...\e[0m"
 
-# دانلود و انتقال اسکریپت به /usr/local/bin
+# Download and move the script to /usr/local/bin
 echo -e "\e[33mDownloading and installing the backup script...\e[0m"
 curl -s -o /usr/local/bin/arm_bm https://raw.githubusercontent.com/arvinmoradi/ArM_Backuper_Marzban/main/backup_marzban_v1.3.sh
 
-# چک کردن موفقیت دانلود
+# Check if the download was successful
 if [ $? -eq 0 ]; then
     echo -e "\e[32mScript downloaded successfully.\e[0m"
 else
@@ -15,16 +15,17 @@ else
     exit 1
 fi
 
-# دادن دسترسی اجرایی به اسکریپت
+# Grant executable permission to the script
 chmod +x /usr/local/bin/arm_bm
 
-# نمایش پیام نصب موفقیت‌آمیز
+# Display successful installation message
 echo -e "\e[32mInstallation complete. You can now use the command 'arm_bm' to run the script.\e[0m"
 
-# اجرای اسکریپت
+# Run the script
 echo -e "\e[33mRunning the script now...\e[0m"
 exec /usr/local/bin/arm_bm
-# چک کردن نصب بودن toilet و نصب آن در صورت نیاز
+
+# Check if the "toilet" package is installed, and install it if necessary
 if ! command -v toilet &> /dev/null; then
     echo -e "\e[33mInstalling toilet for fancy text display...\e[0m"
     sudo apt update
